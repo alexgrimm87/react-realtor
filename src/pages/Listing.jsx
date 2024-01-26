48205ed041d1fa69faa5962b5c5985a247519900
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-26 20:40:49 +0200

    listing page add leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,7 @@ import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from "react-
 import Spinner from "../components/Spinner";
 import Contact from "../components/Contact";
 import {db} from "../firebase";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {Swiper, SwiperSlide} from "swiper/react";
 //import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from "swiper"; //v9
 import {EffectFade, Autoplay, Navigation, Pagination} from "swiper/modules";
@@ -145,7 +146,22 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden">Map</div>
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{height: "100%", width: "100%"}}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.address}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
