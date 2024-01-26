65c3cb3288ead6e8cc9814c3a070d73a04eb2815
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-26 17:16:20 +0200

    listing page add share copy button

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import {doc, getDoc} from "firebase/firestore";
+import {FaShare} from "react-icons/fa";
 import Spinner from "../components/Spinner";
 import {db} from "../firebase";
 import {Swiper, SwiperSlide} from "swiper/react";
@@ -12,6 +13,7 @@ export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   //SwiperCore.use([Autoplay, Navigation, Pagination]); //v9
 
@@ -55,6 +57,24 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12
+        h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
